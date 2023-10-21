117c974629781bb02f8de20819e76421a7507750
Yohan de Rose <yohan.derose@agoda.com>
2023-10-21 22:42:54 +0700

    feat: Adding split rgb and lesser layer/tap delay
    BREAKING CHANGE: Lowering rgb brighness doesn't persist across layers. Also needs layer when shift is on

---
+++ config.h
@@ -0,0 +1,20 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+// layer switching
+#define TAPPING_TERM 120
+#define PERMISSIVE_HOLD
+
+// power management
+#define USB_MAX_POWER_CONSUMPTION 100
+
+// rgb
+#define RGBLIGHT_SPLIT
+#define RGBLIGHT_SLEEP
+#define RGBLIGHT_LAYERS
+#define SPLIT_LAYER_STATE_ENABLE
+#define RGBLIGHT_DEFAULT_MODE RGBLIGHT_MODE_STATIC_LIGHT
+#define RGBLIGHT_DEFAULT_HUE 140 // teal-ish
+#define RGBLIGHT_DEFAULT_VAL 63  // half brightness
+
+#pragma once
---
+++ rules.mk
@@ -0,0 +1,5 @@
+# Copyright 2023 QMK
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+RGBLIGHT_ENABLE = yes
+
