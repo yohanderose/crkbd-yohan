9638f1fc822c3fee7ddf2a9d4f77220033cc6887
Yohan de Rose <yohan.derose@agoda.com>
2023-10-27 11:27:54 +0700

    fix: Reducing space/layer shift thresholds for snappier numbers

--- keymap.c
+++ keymap.c
@@ -19,6 +19,10 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
             return TAPPING_TERM - 50;
 		case LCTL_T(KC_GRV):
             return TAPPING_TERM - 25;
+		case LT(1, KC_SPC):
+			return TAPPING_TERM - 25;
+		case LT(2, KC_SPC):
+			return TAPPING_TERM - 25;
         default:
             return TAPPING_TERM;
     }
